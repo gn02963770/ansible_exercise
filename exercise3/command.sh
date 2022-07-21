1d0acd957131ce71bb1937303a06a961314d5ee3
Ubuntu <ubuntu@ip-172-31-83-32.ec2.internal>
2022-07-21 07:51:27 +0000

    20220721

---
+++ exercise3/command.sh
@@ -0,0 +1,15 @@
+#exercise1
+#ansible -i inventory -m ping web01
+#ansible -i inventory -m ping websrvgrp
+#ansible -i inventory -m ping dbsrvgrp
+#ansible -i inventory -m ping dc_ohio
+#ansible -i inventory -m ping all
+
+#exercise2
+#ansible -i inventory -m yum -a "name=httpd state=present" web01 --become # --become變root
+#ansible -i inventory -m service -a "name=httpd state=started enabled=yes" web01 --become
+#ansible -i inventory -m copy -a "src=index.html dest=/var/www/html/index.html" web01 --become
+
+#exercise3
+ansible-playbook -i inventory web_db.yaml --syntax-check
+ansible-playbook -i inventory web_db.yaml
